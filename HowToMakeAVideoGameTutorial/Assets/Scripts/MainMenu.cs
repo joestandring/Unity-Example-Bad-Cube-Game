d92dfdf1ac06bb5b5301406d2a399b01ddf46fa8
Joe Standring <31478850+joestandring@users.noreply.github.com>
2019-03-17 21:53:14 +0000

    Menu update + options menu
    I made the menu look nicer and added an options menu that can change the games quality, resolution, fullscreen, and master volume.
    I also made a functional pause menu although I messed it up by assuming prefabs would contain all their children (which they evidently do not).

---
+++ HowToMakeAVideoGameTutorial/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // When the play button is clicked Unity will go to the next scene in the index
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit game");
+        Application.Quit();
+    }
+}
---
+++ HowToMakeAVideoGameTutorial/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    public static bool GameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            // if the game is paused and escape is hit, the resume fucntion will fun
+            if(GameIsPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // When resume is clicked, the pasue menu will deactivate, time will resume, and the gameIsPaused variable will be false
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    // When the game is paused, the pause menu will activate, time will freeze, and the gameIsPaused variable will be true
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
